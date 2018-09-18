45f50c79cb4ef63b01ee0fb1abf2235d0f3c1bb8
linucksrox <ericdalyblog@dalydays.com>
2018-09-18 07:13:09 -0400

    Add TextViews to main activity to represent the data (not accessing db yet)

--- app/src/main/java/com/dalydays/android/room/MainActivity.kt
+++ app/src/main/java/com/dalydays/android/room/MainActivity.kt
@@ -2,11 +2,28 @@ package com.dalydays.android.room
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var mDb: UserDatabase? = null
+
+    private lateinit var mDbWorkerThread: DbWorkerThread
+
+    private val mUiHandler = Handler()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mDbWorkerThread = DbWorkerThread("dbWorkerThread")
+        mDbWorkerThread.start()
+
+        mDb = UserDatabase.getInstance(this)
+
+        first_name_tv.text = "Joe"
+        last_name_tv.text = "Miller"
+        age_tv.text = "57"
     }
 }
